5689dd3481ca2d09a4780c0a15de914961d6689a
Matt Dotson <mdotson@microsoft.com>
2018-10-11 11:03:08 -0700

    Allow params.yml to configure AAD SAML for OpsManager

--- install-pcf/azure/tasks/iaas/config-opsman.sh
+++ install-pcf/azure/tasks/iaas/config-opsman.sh
@@ -11,3 +11,14 @@ om-linux --target https://${OPSMAN_DOMAIN_OR_IP_ADDRESS} -k \
   --username "${PCF_OPSMAN_ADMIN}" \
   --password "${PCF_OPSMAN_ADMIN_PASSWORD}" \
   --decryption-passphrase "${PCF_OPSMAN_ADMIN_PASSWORD}"
+
+
+if [[ ${ENABLE_AAD_OPSMAN} ]]; then
+	echo "Configuring AAD SAML for OpsManager"
+	om-linux --target https://${OPSMAN_DOMAIN_OR_IP_ADDRESS} -k \ 
+	  configure-saml-authentication \ 
+	  --saml-bosh-idp-metadata="https://login.microsoftonline.com/${AAD_OPSMAN_TENANTID}/federationmetadata/2007-06/federationmetadata.xml?appid=${AAD_OPSMAN_TENANTID}" \
+	  --saml-idp-metadata="https://login.microsoftonline.com/${AAD_OPSMAN_TENANTID}/federationmetadata/2007-06/federationmetadata.xml?appid=${AAD_OPSMAN_TENANTID}" \
+	  --saml-rbac-admin-group="${AAD_OPSMAN_GROUPID}" \
+	  --saml-rbac-groups-attribute="http://schemas.microsoft.com/ws/2008/06/identity/claims/groups"
+fi

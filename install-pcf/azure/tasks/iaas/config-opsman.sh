5fa51ace82da70e6396e4247bbeddca89be11b60
Matt Dotson <mdotson@microsoft.com>
2018-10-12 16:23:26 -0700

    remove equals for opsman

--- install-pcf/azure/tasks/iaas/config-opsman.sh
+++ install-pcf/azure/tasks/iaas/config-opsman.sh
@@ -17,8 +17,8 @@ if [[ ${ENABLE_OPSMAN_AAD} ]]; then
 	echo "Configuring AAD SAML for OpsManager"
 	om-linux --target "https://${OPSMAN_DOMAIN_OR_IP_ADDRESS}" -k \
 	  configure-saml-authentication \
-	  --saml-bosh-idp-metadata="https://login.microsoftonline.com/${OPSMAN_AAD_TENANTID}/federationmetadata/2007-06/federationmetadata.xml?appid=${OPSMAN_AAD_APPID}" \
-	  --saml-idp-metadata="https://login.microsoftonline.com/${OPSMAN_AAD_TENANTID}/federationmetadata/2007-06/federationmetadata.xml?appid=${OPSMAN_AAD_APPID}" \
-	  --saml-rbac-admin-group="${OPSMAN_AAD_ADMIN_GROUPID}" \
-	  --saml-rbac-groups-attribute="http://schemas.microsoft.com/ws/2008/06/identity/claims/groups"
+	  --saml-bosh-idp-metadata "https://login.microsoftonline.com/${OPSMAN_AAD_TENANTID}/federationmetadata/2007-06/federationmetadata.xml?appid=${OPSMAN_AAD_APPID}" \
+	  --saml-idp-metadata "https://login.microsoftonline.com/${OPSMAN_AAD_TENANTID}/federationmetadata/2007-06/federationmetadata.xml?appid=${OPSMAN_AAD_APPID}" \
+	  --saml-rbac-admin-group "${OPSMAN_AAD_ADMIN_GROUPID}" \
+	  --saml-rbac-groups-attribute "http://schemas.microsoft.com/ws/2008/06/identity/claims/groups"
 fi

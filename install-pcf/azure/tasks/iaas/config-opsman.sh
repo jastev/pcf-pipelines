81eaa4c72f853a5edf99077f1777f3f0a7fa9782
Matt Dotson <mdotson@microsoft.com>
2018-10-12 16:27:57 -0700

    whitespace for opsman

--- install-pcf/azure/tasks/iaas/config-opsman.sh
+++ install-pcf/azure/tasks/iaas/config-opsman.sh
@@ -15,8 +15,7 @@ om-linux --target https://${OPSMAN_DOMAIN_OR_IP_ADDRESS} -k \
 
 if [[ ${ENABLE_OPSMAN_AAD} ]]; then
 	echo "Configuring AAD SAML for OpsManager"
-	om-linux --target "https://${OPSMAN_DOMAIN_OR_IP_ADDRESS}" -k \
-		configure-saml-authentication \
+	om-linux --target "https://${OPSMAN_DOMAIN_OR_IP_ADDRESS}" -k configure-saml-authentication \
 	  	--saml-bosh-idp-metadata "https://login.microsoftonline.com/${OPSMAN_AAD_TENANTID}/federationmetadata/2007-06/federationmetadata.xml?appid=${OPSMAN_AAD_APPID}" \
 	  	--saml-idp-metadata "https://login.microsoftonline.com/${OPSMAN_AAD_TENANTID}/federationmetadata/2007-06/federationmetadata.xml?appid=${OPSMAN_AAD_APPID}" \
 		--saml-rbac-admin-group "${OPSMAN_AAD_ADMIN_GROUPID}" \

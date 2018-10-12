4b0d5f2fc94bc3d8a4d3e5067a19b155a602e40a
David Tesar <david.tesar@microsoft.com>
2018-10-12 08:37:53 -0700

    debug

--- install-pcf/azure/tasks/create-infrastructure/task.sh
+++ install-pcf/azure/tasks/create-infrastructure/task.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set -ex
 
 # Copy base template with no clobber if not using the base template
 if [[ ! ${AZURE_PCF_TERRAFORM_TEMPLATE} == "c0-azure-base" ]]; then
@@ -7,7 +7,7 @@ if [[ ! ${AZURE_PCF_TERRAFORM_TEMPLATE} == "c0-azure-base" ]]; then
 fi
 
 # Get ert subnet if multi-resgroup
-if [ -z "${AZURE_MULTI_RESGROUP_NETWORK}" ]; then
+if [[ "${AZURE_MULTI_RESGROUP_NETWORK}" != "" ]]; then
 	az login --service-principal -u ${AZURE_CLIENT_ID} -p ${AZURE_CLIENT_SECRET} --tenant ${AZURE_TENANT_ID}
 	az account set --subscription ${AZURE_SUBSCRIPTION_ID}
 	ERT_SUBNET_CMD="az network vnet subnet list -g network-core --vnet-name vnet-pcf --output json | jq '.[] | select(.name == \"ert\") | .id' | tr -d '\"'"
@@ -47,6 +47,11 @@ fi
 echo "=============================================================================================="
 echo "Executing Terraform Plan ..."
 echo "=============================================================================================="
+
+echo "Storage Acount Name: " $TERRAFORM_AZURE_STORAGE_ACCOUNT_NAME
+echo "container" ${TERRAFORM_AZURE_STORAGE_CONTAINER_NAME} 
+echo "key ${TERRAFORM_AZURE_STATEFILE_NAME}" 
+echo "access_key ${TERRAFORM_AZURE_STORAGE_ACCESS_KEY}"
 
 terraform init -backend=true -backend-config="storage_account_name=${TERRAFORM_AZURE_STORAGE_ACCOUNT_NAME}" -backend-config="container_name=${TERRAFORM_AZURE_STORAGE_CONTAINER_NAME}" -backend-config="key=${TERRAFORM_AZURE_STATEFILE_NAME}" -backend-config="access_key=${TERRAFORM_AZURE_STORAGE_ACCESS_KEY}" "pcf-pipelines/install-pcf/azure/terraform/${AZURE_PCF_TERRAFORM_TEMPLATE}"
 

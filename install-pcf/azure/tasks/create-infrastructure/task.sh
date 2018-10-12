739c5fa6caa28d84f32c7c09f201eb30bc0826d6
Matt Dotson <mdotson@microsoft.com>
2018-10-12 15:28:15 -0700

    changed terraform apply to use plan

--- install-pcf/azure/tasks/create-infrastructure/task.sh
+++ install-pcf/azure/tasks/create-infrastructure/task.sh
@@ -51,8 +51,6 @@ echo "==========================================================================
 
 terraform init -backend=true -backend-config="storage_account_name=${TERRAFORM_AZURE_STORAGE_ACCOUNT_NAME}" -backend-config="container_name=${TERRAFORM_AZURE_STORAGE_CONTAINER_NAME}" -backend-config="key=${TERRAFORM_AZURE_STATEFILE_NAME}" -backend-config="access_key=${TERRAFORM_AZURE_STORAGE_ACCESS_KEY}" "pcf-pipelines/install-pcf/azure/terraform/${AZURE_PCF_TERRAFORM_TEMPLATE}"
 
-terraform init
-
 terraform plan \
   -var "arm_subscription_id=${AZURE_SUBSCRIPTION_ID}" \
   -var "arm_client_id=${AZURE_CLIENT_ID}" \
@@ -93,4 +91,4 @@ echo "==========================================================================
 echo "Executing Terraform Apply ..."
 echo "=============================================================================================="
 
-terraform apply -auto-approve
+terraform apply "terraform.tfplan" -auto-approve

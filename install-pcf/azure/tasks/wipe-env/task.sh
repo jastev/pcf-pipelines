2dc065a7f20e569ff4fbd01cd45513f872846d05
Matt Dotson <mdotson@microsoft.com>
2018-10-13 14:07:43 -0700

    use terraform destroy for wipe-env

--- install-pcf/azure/tasks/wipe-env/task.sh
+++ install-pcf/azure/tasks/wipe-env/task.sh
@@ -35,7 +35,7 @@ function delete-infrastructure() {
   #terraform init "pcf-pipelines/install-pcf/azure/terraform/${AZURE_PCF_TERRAFORM_TEMPLATE}"
   terraform init -backend=true -backend-config="storage_account_name=${TERRAFORM_AZURE_STORAGE_ACCOUNT_NAME}" -backend-config="container_name=${TERRAFORM_AZURE_STORAGE_CONTAINER_NAME}" -backend-config="key=${TERRAFORM_AZURE_STATEFILE_NAME}" -backend-config="access_key=${TERRAFORM_AZURE_STORAGE_ACCESS_KEY}" "pcf-pipelines/install-pcf/azure/terraform/${AZURE_PCF_TERRAFORM_TEMPLATE}"
 
-  terraform destroy -auto-approve \
+  terraform destroy -force \
     -var "subscription_id=${AZURE_SUBSCRIPTION_ID}" \
     -var "client_id=${AZURE_CLIENT_ID}" \
     -var "client_secret=${AZURE_CLIENT_SECRET}" \

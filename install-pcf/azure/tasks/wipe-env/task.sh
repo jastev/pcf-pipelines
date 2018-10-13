a6ffe43521d4fd78cbdcccf1b49121bef611b41c
Matt Dotson <mdotson@microsoft.com>
2018-10-13 14:12:10 -0700

    use terraform destroy for wipe-env

--- install-pcf/azure/tasks/wipe-env/task.sh
+++ install-pcf/azure/tasks/wipe-env/task.sh
@@ -68,6 +68,7 @@ function delete-infrastructure() {
     -var "vm_admin_username=dontcare" \
     -var "vm_admin_public_key=dontcare" \
 	-var "arm_client_id=dontcare" \
+	-var "arm_client_secret=dontcare" \
     -var "azure_multi_resgroup_network=dontcare" \
     -var "azure_multi_resgroup_pcf=dontcare" \
     -var "azure_opsman_priv_ip=dontcare" \

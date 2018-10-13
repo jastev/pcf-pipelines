acd0714610ee278b9967a52f819377cd00031628
Matt Dotson <mdotson@microsoft.com>
2018-10-13 13:43:11 -0700

    use terraform plan for wipe-env

--- install-pcf/azure/tasks/wipe-env/task.sh
+++ install-pcf/azure/tasks/wipe-env/task.sh
@@ -35,7 +35,7 @@ function delete-infrastructure() {
   #terraform init "pcf-pipelines/install-pcf/azure/terraform/${AZURE_PCF_TERRAFORM_TEMPLATE}"
   terraform init -backend=true -backend-config="storage_account_name=${TERRAFORM_AZURE_STORAGE_ACCOUNT_NAME}" -backend-config="container_name=${TERRAFORM_AZURE_STORAGE_CONTAINER_NAME}" -backend-config="key=${TERRAFORM_AZURE_STATEFILE_NAME}" -backend-config="access_key=${TERRAFORM_AZURE_STORAGE_ACCESS_KEY}" "pcf-pipelines/install-pcf/azure/terraform/${AZURE_PCF_TERRAFORM_TEMPLATE}"
 
-  terraform destroy -force \
+  terraform plan -destroy \
     -var "subscription_id=${AZURE_SUBSCRIPTION_ID}" \
     -var "client_id=${AZURE_CLIENT_ID}" \
     -var "client_secret=${AZURE_CLIENT_SECRET}" \
@@ -78,7 +78,10 @@ function delete-infrastructure() {
     -var "om_disk_size_in_gb=50" \
 #    -state "${ROOT}/terraform-state/terraform.tfstate" \
 #    -state-out "${ROOT}/terraform-state-output/terraform.tfstate" \
+	-out terraform.tfplan \
     "pcf-pipelines/install-pcf/azure/terraform/${AZURE_PCF_TERRAFORM_TEMPLATE}"
+
+  terraform apply -auto-approve "terraform.tfplan"
 }
 
 function main() {

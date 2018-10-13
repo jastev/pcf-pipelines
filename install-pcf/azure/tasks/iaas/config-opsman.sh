9d6871514749297733ff4dea53f4b8511357175c
Matt Dotson <mdotson@microsoft.com>
2018-10-13 15:48:30 -0700

    changing opsman to use authentication_mode

--- install-pcf/azure/tasks/iaas/config-opsman.sh
+++ install-pcf/azure/tasks/iaas/config-opsman.sh
@@ -6,14 +6,8 @@ echo "Configuring OpsManager @ https://${OPSMAN_DOMAIN_OR_IP_ADDRESS} ..."
 echo "=============================================================================================="
 
 #Configure Opsman
-om-linux --target https://${OPSMAN_DOMAIN_OR_IP_ADDRESS} -k \
-  configure-authentication \
-  --username "${PCF_OPSMAN_ADMIN}" \
-  --password "${PCF_OPSMAN_ADMIN_PASSWORD}" \
-  --decryption-passphrase "${PCF_OPSMAN_ADMIN_PASSWORD}"
-
-
-if [[ ${ENABLE_OPSMAN_AAD} ]]; then
+if [[ ${AUTHENTICATION_MODE == "saml" }]]
+#if [[ ${ENABLE_OPSMAN_AAD} ]]; then
 	echo "Configuring AAD SAML for OpsManager"
 	om-linux --target "https://${OPSMAN_DOMAIN_OR_IP_ADDRESS}" -k \
 		--username "${PCF_OPSMAN_ADMIN}" \
@@ -23,5 +17,12 @@ if [[ ${ENABLE_OPSMAN_AAD} ]]; then
 	  	--saml-idp-metadata "https://login.microsoftonline.com/${OPSMAN_AAD_TENANTID}/federationmetadata/2007-06/federationmetadata.xml?appid=${OPSMAN_AAD_APPID}" \
 		--saml-rbac-admin-group "${OPSMAN_AAD_ADMIN_GROUPID}" \
 	  	--saml-rbac-groups-attribute "http://schemas.microsoft.com/ws/2008/06/identity/claims/groups" \
+		--decryption-passphrase "${PCF_OPSMAN_ADMIN_PASSWORD}"
+
+elif [[ ${AUTHENTICATION_MODE == "internal" }]]
+	om-linux --target https://${OPSMAN_DOMAIN_OR_IP_ADDRESS} -k \
+		configure-authentication \
+		--username "${PCF_OPSMAN_ADMIN}" \
+		--password "${PCF_OPSMAN_ADMIN_PASSWORD}" \
 		--decryption-passphrase "${PCF_OPSMAN_ADMIN_PASSWORD}"
 fi
